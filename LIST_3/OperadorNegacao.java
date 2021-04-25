a6c31ae873a5f86937cd9465a33305e8d5294dae
Jéssica <73039194+JEZIGA01@users.noreply.github.com>
2021-04-24 21:41:52 -0300

    exercicios_faculdade

---
+++ LIST_3/OperadorNegacao.java
@@ -0,0 +1,16 @@
+//JÉSSICA FERNANDA AQUILES LÚCIO RA:2021375 
+
+
+
+public class OperadorNegacao {
+	
+	public static void main (String[] args) {
+		
+		int a = 10;
+		int b = 20;
+		
+		System.out.println( !(a > b)   );
+		System.out.println(!( a < b )  );
+	}
+}
+
---
+++ LIST_3/OperadorTernario.java
@@ -0,0 +1,27 @@
+//JÉSSICA FERNANDA AQUILES LÚCIO RA:2021375 
+
+
+public class OperadorTernario {
+	
+	public static void main (String[] args) {
+		
+		int a = (int)(Math.random() * 100);
+		int b = (int)(Math.random() * 100);
+		
+		String resultado = ( a < b ) ? " A menor que B " :" A maior ou igual a B" ;
+		
+        // System.out.println(a);
+       //  System.out.println(b);
+		System.out.println(resultado);
+			
+			
+			
+			
+			
+			
+		
+				
+		
+	}
+}
+
