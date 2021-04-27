5764a09a2b20193f7c3b06096ed1bb82c4b2678e
Jéssica <73039194+JEZIGA01@users.noreply.github.com>
2021-04-27 13:40:10 -0300

    Add files via upload

---
+++ LIST_4/AprovadoReprovado.java
@@ -0,0 +1,56 @@
+/*
+Crie um arquivo chamado AprovadoReprovado.java. 
+* Solicite ao usuário que digite o valor para a variável nota. 
+* O programa deve testar o valor digitado pelo usuário e caso não esteja na faixa de 0 até 10 (inclusos), 
+* exibir a mensagem VALOR INVALIDO e finalizer o programa. 
+* Caso o valor digitado seja válido, exibir a mensagem REPROVADO quando a nota do aluno for menor do que 6 ou APROVADO caso contrário
+ 
+ */
+
+
+
+import java.util.Scanner;
+
+public class AprovadoReprovado {
+	
+	public static void main (String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+		
+		//int nota = (int)(Math.random() * 10);
+		
+		System.out.println("Digite uma nota de 0 a 10");
+		
+		int nota = sc.nextInt();
+		
+			if (nota > 10 ){
+			
+			         
+			         System.out.println("Valor invalido !");
+			
+			
+			
+			}
+			
+			else if (nota >=6){
+
+                    System.out.println("Aprovado");
+			
+				
+				}
+				
+				
+				else{
+					System.out.println("Reprovado");
+					}
+				
+		
+				
+		
+	
+	
+		
+		//System.out.println(valor);
+	}
+}
+
