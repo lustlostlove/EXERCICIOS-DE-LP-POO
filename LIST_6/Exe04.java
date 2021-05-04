6d777131d7c790ee11feabc37a6e3ef075f5b91e
Jéssica <73039194+JEZIGA01@users.noreply.github.com>
2021-05-03 21:46:01 -0300

    Add files via upload

---
+++ LIST_6/Exe04.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+
+public class Exe04 {
+	
+	public static void main (String[] args) {
+		
+		
+		Scanner sc = new Scanner (System.in);
+		
+		int N = 1;
+		
+		 N = sc.nextInt();
+		
+		for ( int i = 1 ; i <= N ; i++){
+			
+			System.out.print(i + " ");
+			
+		
+	}
+}
+
+
+}
+
