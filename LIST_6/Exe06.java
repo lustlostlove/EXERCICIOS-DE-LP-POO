b60c7a4b5f047092b240f4003e25a5e1486bb28a
Jéssica <73039194+JEZIGA01@users.noreply.github.com>
2021-05-04 22:02:31 -0300

    Add files via upload

---
+++ LIST_6/Exe06.java
@@ -0,0 +1,19 @@
+
+/*
+ *  Escreva um programa em Java que calcule e imprima a tabuada do 8 (1 a 10).
+ * 
+ * */
+public class Exe06 {
+	
+	public static void main (String[] args) {
+		
+		 int n = 8 ;
+		 for ( int i = 1 ; i <= 10 ; i++){
+			
+			
+			int c = n * i ;
+			System.out.println( i  + " x 8 = " + c );
+	}
+
+}
+}
