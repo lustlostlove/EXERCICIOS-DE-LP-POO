36899ad0c33023169e65d3bc897cf03efc564260
Jéssica <73039194+JEZIGA01@users.noreply.github.com>
2021-06-20 20:51:36 -0300

    EXERCICIO 15

---
+++ LIST_6/Exe15.java
@@ -0,0 +1,40 @@
+/*
+15) O mesmo exercício anterior, mas agora, considere que o segundo valor lido poderá ser maior ou menor que o primeiro valor lido,
+*  ou seja, deve-se testá-los.
+ */
+
+import java.util.Scanner;
+
+public class Exe15 {
+	
+	public static void main (String[] args) {
+		 
+		 Scanner sc = new Scanner (System.in);
+		 	
+		 	int soma = 0 , valor=0, valorb = 0;
+		 	
+		 			 valor = sc.nextInt();
+		 			 valorb =sc.nextInt();
+		 			 
+		 			 if (valorb > valor || valorb < valor){
+						  System.out.println("");
+		 	  System.out.println("");
+		 	 for (int i = valor;  i <= valorb; i++){
+				
+				  System.out.print(i + " ");
+                  soma+=i;
+                  
+
+				 }
+ 
+                   System.out.print("");
+           
+             				 System.out.println(" \n Total: "+soma);
+						 }
+		 	
+           else{
+			    System.out.print("O primeiro valor tem que ser maior ou menor que o segundo valor");
+			   }
+		   }
+	   }
+   
