e4d9f83b09aea1e7d7108a68760055c2650b4532
Jéssica <73039194+JEZIGA01@users.noreply.github.com>
2021-06-20 19:27:24 -0300

    EXERCICIO 12

---
+++ LIST_6/Exe12.java
@@ -0,0 +1,28 @@
+/*
+ Escreva um programa em Java para ler 10 números e ao final da leitura escrever a soma total dos 10 números lidos.
+ */
+
+
+
+import java.util.Scanner;
+
+public class Exe12 {
+	
+	public static void main (String[] args) {
+		 
+		 Scanner sc = new Scanner (System.in);
+		 
+		int soma = 0 , valor = 0;
+		
+		 for (int i = 1; i <= 10; i++){
+
+             valor = sc.nextInt();
+             
+             soma+=valor;
+	 }
+	  System.out.println("\n");
+	 System.out.println("Total "+soma);
+	 
+	}
+}
+
