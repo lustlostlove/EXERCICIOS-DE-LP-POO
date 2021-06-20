49a582129745e8902b6442fcff207d9e45625c21
Jéssica <73039194+JEZIGA01@users.noreply.github.com>
2021-06-20 20:19:24 -0300

    EXERCICIO 14

--- LIST_6/Exe14.java
+++ LIST_6/Exe14.java
@@ -1,7 +1,4 @@
-/*
- 14) Escreva um programa em Java para ler 2 valores, calcular e escrever a soma dos inteiros existentes entre os 2 valores lidos 
- * (incluindo os valores lidos na soma). Considere que o segundo valor lido será sempre maior que o primeiro valor lido*/
- 
+/*14) Escreva um programa em Java para ler 2 valores, calcular e escrever a soma dos inteiros existentes entre os 2 valores lidos (incluindo os valores lidos na soma). Considere que o segundo valor lido será sempre maior que o primeiro valor lido.*/
 import java.util.Scanner;
 
 public class Exe14 {
