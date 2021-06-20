a100c784b24efac0b011cb369f2ba0d3e243921a
Jéssica <73039194+JEZIGA01@users.noreply.github.com>
2021-06-20 20:11:22 -0300

    EXERCICIOS 13

--- LIST_6/Exe13.java
+++ LIST_6/Exe13.java
@@ -1,6 +1,6 @@
 /*
- Escreva um programa em Java para ler 10 números. Todos os números lidos com valor inferior a 40 devem ser somados. Escreva o valor final da soma efetuada
- */
+ 14) Escreva um programa em Java para ler 2 valores, calcular e escrever a soma dos inteiros existentes entre os 2 valores lidos 
+ * (incluindo os valores lidos na soma). Considere que o segundo valor lido será sempre maior que o primeiro valor lido*/
  
 import java.util.Scanner;
 
@@ -9,20 +9,26 @@ public class Exe13 {
 	public static void main (String[] args) {
 		 
 		 Scanner sc = new Scanner (System.in);
-		 
-		int soma = 0 , valor = 0;
-		
-		 for (int i = 1; i <= 10; i++){
+		 	
+		 	int soma = 0 , valor=0, valorb = 0;
+		 	
+		 			 valor = sc.nextInt();
+		 			 valorb =sc.nextInt();
+		 	 System.out.println("");
+		 	  System.out.println("");
+		 	 for (int i = valor;  i <= valorb; i++){
+				
+				  System.out.print(i + " ");
+                  soma+=i;
+                  
 
-             valor = sc.nextInt();
-             if(valor < 40){
-				 soma+=valor;
 				 }
+ 
+                   System.out.print("");
+           
+             				 System.out.println(" \n Total: "+soma);
+           
              
-	 }
-	  System.out.println("\n");
-	 System.out.println("Total "+soma);
 	 
 	}
 }
-
