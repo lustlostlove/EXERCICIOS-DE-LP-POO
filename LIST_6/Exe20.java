637565f1b10eaa8f20e7ff26e231ad4f4ce3987b
Jéssica <73039194+JEZIGA01@users.noreply.github.com>
2021-06-21 19:41:10 -0300

    EXERCICIO 20

---
+++ LIST_6/Exe20.java
@@ -0,0 +1,40 @@
+/*
+20) Escreva um programa em Java que leia 100 valores e no final, escreva o maior e o menor valor lido.
+ */
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Exe20 {
+	public static void main (String[] args) {
+		
+		 Random random = new Random();
+		 Scanner sc = new Scanner (System.in);
+		 
+		 int maior =0,menor =0, numero=0;
+		
+	for(int i = 1; i <= 100; i++){
+			numero =random.nextInt(100);
+			
+			System.out.print(numero+" ");
+			
+			if(i ==  1){
+				menor = numero;
+				  maior = numero;	
+			}
+		if(numero > maior){
+				maior = numero;
+			}
+			  if(numero < menor){
+				   menor = numero;
+			}
+			
+		}
+		System.out.println();
+		System.out.println();
+		System.out.println("Numero maior: " + maior);
+		System.out.println("Numero menor: " + menor);
+	 }
+
+	}
+
