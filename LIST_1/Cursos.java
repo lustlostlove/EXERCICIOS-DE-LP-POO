a1aa0912f25699a6dba4ba1466942a7389514dbc
Rohan Sharma <34921167+lustlostlove@users.noreply.github.com>
2021-08-03 03:33:22 +0530

    Delete LIST_1 directory

--- LIST_1/Cursos.java
+++
@@ -1,9 +0,0 @@
-
-public class Cursos {
-	
-	public static void main (String[] args) {
-		System.out.println("\tF01 Logica de Programacao\n\tF02 Linguagem de Programacao\n\tF03 Programacao Orientada a Objeto"); 
-		
-	}
-}
-
--- LIST_1/DuasMensagens.java
+++
@@ -1,9 +0,0 @@
-
-public class DuasMensagens {
-	public static void main (String[] args) {
-		
-		System.out.println("Hello World 1  \nHello World 2");
-		
-	}
-}
-
